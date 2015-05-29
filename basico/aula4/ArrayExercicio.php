bbb6bd9550f2333cfb91c89350eb7598ae465d34
Vinicius Ruani <Viniciusruani@gmail.com>
2015-05-29 12:13:55 -0300

    Correção nas palavras
    Corrigindo algumas palavras =D

--- basico/aula4/ArrayExercicio.php
+++ basico/aula4/ArrayExercicio.php
@@ -3,8 +3,8 @@
 Criar arrays e fazer:
 
 1 - Acrescentar um valor no final do array.
-2 - Ordar o array.
-3 - Dividar o array.
+2 - Ordenar o array.
+3 - Dividir o array.
 4 - Juntar os arrays divididos.
 
 
